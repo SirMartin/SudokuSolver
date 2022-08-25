333617598ead00def71234082bfe181d8168f340
SirMartin <sirmartin@gmail.com>
2022-08-25 09:32:05 +0200

    Finished first version with parsing and printing the sudoku in the console.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SudokuSolver;
+
 Console.WriteLine("Hello, World!");
+
+var content = "8,2,*,*,4,*,*,6,*;" +
+            "*,*,1,6,*,*,8,9,*;" +
+            "*,*,9,8,3,1,5,*,7;" +
+            "4,9,*,1,5,7,*,*,*;" +
+            "*,*,*,*,*,*,*,*,*;" +
+            "*,5,3,*,*,4,*,*,*;" +
+            "9,6,*,4,1,5,*,*,8;" +
+            "1,*,*,7,6,3,2,*,*;" +
+            "3,*,*,*,2,8,*,5,1;";
+
+var solver = new Solver(content);
+solver.PrintSudoku();
---
+++ Solver.cs
@@ -0,0 +1,46 @@
+﻿namespace SudokuSolver
+{
+    internal class Solver
+    {
+        public int?[,] Sudoku { get; set; }
+
+        public Solver(string sudokuContent)
+        {
+            Sudoku = ParseSudoku(sudokuContent);
+        }
+
+        private int?[,] ParseSudoku(string sudokuContent)
+        {
+            var sudoku = new int?[9, 9];
+
+            for (var r = 0; r < 9; r++)
+            {
+                var row = sudokuContent.Split(";")[r];
+                var splittedRow = row.Split(",");
+                for (var c = 0; c < 9; c++)
+                {
+                    sudoku[r, c] = splittedRow[c] != "*" ? int.Parse(splittedRow[c]) : null;
+                }
+            }
+
+            return sudoku;
+        }
+
+        public void PrintSudoku()
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                for (var j = 0; j < 9; j++)
+                {
+                    Console.Write($" {Sudoku[i, j]} ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        internal void Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
