a44468a6cffe68ed379e40552a24fa8c1140fc09
SirMartin <sirmartin@gmail.com>
2022-08-25 09:54:00 +0200

    Added way to resolve rows, columns and squares separaterely and tests

--- Solver.cs
+++ Solver.cs
@@ -38,6 +38,52 @@
             }
         }
 
+        internal static int[] SolveColumn(int?[] col)
+        {
+            var possibleSolutions = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < col.Length; i++)
+            {
+                if (col[i] != null)
+                {
+                    possibleSolutions.Remove((int)col[i]);
+                }
+            }
+
+            return possibleSolutions.ToArray();
+        }
+
+
+        internal static int[] SolveRow(int?[] row)
+        {
+            var possibleSolutions = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < row.Length; i++)
+            {
+                if (row[i] != null)
+                {
+                    possibleSolutions.Remove((int)row[i]);
+                }
+            }
+
+            return possibleSolutions.ToArray();
+        }
+
+        internal static int[] SolveSquare(int?[,] square)
+        {
+            var possibleSolutions = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (square[i, j] != null)
+                    {
+                        possibleSolutions.Remove((int)square[i, j]);
+                    }
+                }
+            }
+
+            return possibleSolutions.ToArray();
+        }
+
         internal void Solve()
         {
             throw new NotImplementedException();
--- Tests/SudokuTests.cs
+++ Tests/SudokuTests.cs
@@ -1,7 +1,9 @@
 using NUnit.Framework;
+using SudokuSolver;
 
 namespace Tests
 {
+    [TestFixture]
     public class SudokuTests
     {
         [SetUp]
@@ -10,9 +12,78 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void SudokuColumn()
         {
-            Assert.Pass();
+            var col = new int?[] { 1, 2, 3, null, 5, 6, 7, 8, 9 };
+            int[] possibleSolutions = Solver.SolveColumn(col);
+
+            Assert.AreEqual(1, possibleSolutions.Length);
+            Assert.AreEqual(new int[] { 4 }, possibleSolutions);
+        }
+
+        [Test]
+        public void SudokuColumnMultipleSolutions()
+        {
+            var col = new int?[] { null, 2, 3, null, 5, 6, null, 8, null };
+            int[] possibleSolutions = Solver.SolveColumn(col);
+
+            Assert.AreEqual(4, possibleSolutions.Length);
+            Assert.AreEqual(new int[] { 1, 4, 7, 9 }, possibleSolutions);
+        }
+
+        [Test]
+        public void SudokuRow()
+        {
+            var row = new int?[] { 1, 2, 3, null, 5, 6, 7, 8, 9 };
+            int[] possibleSolutions = Solver.SolveRow(row);
+
+            Assert.AreEqual(1, possibleSolutions.Length);
+            Assert.AreEqual(new int[] { 4 }, possibleSolutions);
+        }
+
+        [Test]
+        public void SudokuRowMultipleSolutions()
+        {
+            var row = new int?[] { null, 2, 3, null, 5, 6, null, 8, null };
+            int[] possibleSolutions = Solver.SolveRow(row);
+
+            Assert.AreEqual(4, possibleSolutions.Length);
+            Assert.AreEqual(new int[] { 1, 4, 7, 9 }, possibleSolutions);
+        }
+
+
+        [Test]
+        public void SudokuSquare()
+        {
+            var square = new int?[3, 3] { { 1, 2, 3 }, { null, 5, 6 }, { 7, 8, 9 } };
+            int[] possibleSolutions = Solver.SolveSquare(square);
+
+            Assert.AreEqual(1, possibleSolutions.Length);
+            Assert.AreEqual(new int[] { 4 }, possibleSolutions);
+        }
+
+
+        [Test]
+        public void SudokuSquareMultipleSolutions()
+        {
+            var square = new int?[3, 3] { { null, 2, 3 }, { null, 5, 6 }, { null, 8, null } };
+            int[] possibleSolutions = Solver.SolveSquare(square);
+
+            Assert.AreEqual(4, possibleSolutions.Length);
+            Assert.AreEqual(new int[] { 1, 4, 7, 9 }, possibleSolutions);
+        }
+
+        private string GetEasySudokuContent()
+        {
+            return "8,2,*,*,4,*,*,6,*;" +
+                   "*,*,1,6,*,*,8,9,*;" +
+                   "*,*,9,8,3,1,5,*,7;" +
+                   "4,9,*,1,5,7,*,*,*;" +
+                   "*,*,*,*,*,*,*,*,*;" +
+                   "*,5,3,*,*,4,*,*,*;" +
+                   "9,6,*,4,1,5,*,*,8;" +
+                   "1,*,*,7,6,3,2,*,*;" +
+                   "3,*,*,*,2,8,*,5,1;";
         }
     }
 }
